8be1bb0538fc3500cd666ca5fa15d54e211d9fa3
fatimashahzad00 <fatimashahzad062@gmail.com>
2023-03-15 16:49:56 +0500

    panaverse-chakra-ui

---
+++ panaverse/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Banner from "./components/Header";
+
+
+export default function Home() {
+  return (
+    <>
+   <Header/>
+   {/* <Banner /> */}
+   <Footer/>
+   </>
+  )
+}
